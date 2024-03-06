b428c6ca914139d0301bbd8c3b2e79660404219b
Dante Tyler <132313624+HHA0998@users.noreply.github.com>
2024-03-06 16:55:16 +1100

    2d array hooray!

--- MazeGame/Game.cs
+++ MazeGame/Game.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace MazeGame
 {
@@ -19,12 +21,35 @@ namespace MazeGame
         int yPos = 10;
 
         //world size
-        int length = 25;
-        int height = 25;
+        //int length = 25;
+        //int height = 25;
+        string[,] levelMap =
+        {
+          { "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"},
+          { "0", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "0"},
+          { "0", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "0"},
+          { "0", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "0"},
+          { "0", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "0"},
+          { "0", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "0"},
+          { "0", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "0"},
+          { "0", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "0"},
+          { "0", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "0"},
+          { "0", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "0"},
+          { "0", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "0"},
+          { "0", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "0"},
+          { "0", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "0"},
+          { "0", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "0"},
+          { "0", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "0"},
+          { "0", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "0"},
+          { "0", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "0"},
+          { "0", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "0"},
+          { "0", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "0"},
+          { "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"},
+        };
 
         //boarder
-        int xBoarder = 24;
-        int yBoarder = 24;
+        int xBoarder = 0;
+        int yBoarder = 0;
 
         bool exit = false;
 
@@ -32,9 +57,10 @@ namespace MazeGame
         public Game()
         {
             //GAME LOOP
+            xBoarder = levelMap.GetLength(0) - 1;
+            yBoarder = levelMap.GetLength(1) - 1;
 
-
-           
+            Draw();
             while (exit == false)
             {
                 //Call functions here
@@ -61,13 +87,25 @@ namespace MazeGame
                     yDirInput = -1;
                     break;
                 case ConsoleKey.S:
-                    yDirInput = 1;
+                    yDirInput = +1;
                     break;
                 case ConsoleKey.A:
                     xDirInput = -1;
                     break;
                 case ConsoleKey.D:
-                    xDirInput = 1;
+                    xDirInput = +1;
+                    break;
+                case ConsoleKey.UpArrow:
+                    yDirInput = -1;
+                    break;
+                case ConsoleKey.DownArrow:
+                    yDirInput = +1;
+                    break;
+                case ConsoleKey.LeftArrow:
+                    xDirInput = -1;
+                    break;
+                case ConsoleKey.RightArrow:
+                    xDirInput = +1;
                     break;
             }
         }
@@ -81,8 +119,27 @@ namespace MazeGame
             //reset diacrection input
             yDirInput = 0;
             xDirInput = 0;
-        }
 
+            if (xPos < 1)
+            {
+                xPos = 1;
+            }
+            if (yPos < 1)
+            {
+                yPos = 1;
+            }
+            if (xPos > 18)
+            {
+                xPos = levelMap.GetLength(0) - 2;
+            }
+            if (yPos > 18)
+            {
+                yPos = levelMap.GetLength(1) - 2;
+            }
+
+        }
+        // length or xPos = levelMap.GetLength(0)
+        // height or yPos = levelMap.GetLength(1)
 
 
         void Draw()
@@ -92,9 +149,9 @@ namespace MazeGame
 
             string screen = "";
 
-            for (int y = 0; y < length; y++)
+            for (int y = 0; y < levelMap.GetLength(1); y++)
             {
-                for (int x = 0; x < height; x++)
+                for (int x = 0; x < levelMap.GetLength(0); x++)
                 {
                     // draw player
                     if (yPos == y && xPos == x)
@@ -102,14 +159,22 @@ namespace MazeGame
                         screen += " #";
                     }
                     // draw boarder
-                    else if (xBoarder == x || yBoarder == y || x == 0 || y == 0)
+                    else if (yBoarder == y || y == 0)
                     {
                         screen += "[]";
+                    }
+                    else if (x == 0)
+                    {
+                        screen += " |";
+                    }
+                    else if (xBoarder == x)
+                    {
+                        screen += "| ";
                     }
                     // draw space
                     else
                     {
-                        screen += "  ";
+                        screen += " " + levelMap[y,x];
                     }
 
                 }
@@ -117,9 +182,31 @@ namespace MazeGame
             }
             Console.WriteLine(screen);
 
-            Console.WriteLine("Ypos = " + yPos + " : " + "Xpos = " + xPos);
+            WriteColor("Ypos = [" + yPos + "] : " + "Xpos = [" + xPos + "] ", ConsoleColor.Green);
         }
 
+
+        // usage: WriteColor("This is my [message] with inline [color] changes.", ConsoleColor.Yellow);
+        static void WriteColor(string message, ConsoleColor color)
+        {
+            var pieces = Regex.Split(message, @"(\[[^\]]*\])");
+
+            for (int i = 0; i < pieces.Length; i++)
+            {
+                string piece = pieces[i];
+
+                if (piece.StartsWith("[") && piece.EndsWith("]"))
+                {
+                    Console.ForegroundColor = color;
+                    piece = piece.Substring(1, piece.Length - 2);
+                }
+
+                Console.Write(piece);
+                Console.ResetColor();
+            }
+
+            Console.WriteLine();
+        }
 
 
     }
